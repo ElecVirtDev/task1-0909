761738eb87531f94c2f597f4d7ea25f597e88d43
ElecVirtDev <evd94623@gmail.com>
2025-09-04 17:23:23 +0900

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import "./assets/css/glightbox.min.css";
 import "./assets/css/scrollCue.css";
 import "./assets/css/spacing.css";
 import "./assets/css/responsive.css";
+import "./assets/css/main.css";
 
 // JS 불러오기
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
