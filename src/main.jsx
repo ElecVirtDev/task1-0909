b95e59c7bafdbe014e1328f1aebebc68f8e3c4e1
ElecVirtDev <evd94623@gmail.com>
2025-09-04 15:16:09 +0900

    build(deps): update package-lock.json, App.jsx, main.jsx and vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./js/modernizr.min.js";
+import "./js/jquery-3.6.3.min.js";
+import "./js/popper.min.js";
+import "./js/mixitup.min.js";
+import "./js/glightbox.min.js";
+import "./js/jquery.waypoints.min.js";
+import "./js/jquery.counterup.min.js";
+import "./js/vanilla-tilt.min.js";
+import "./js/swiper-bundle.js";
+import "./js/scrollCue.min.js";
+import "./js/wow.js";
+import "./js/mobile-menu.js";
+import "./js/main.js";
 import "./assets/css/index.css";
+
+import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter basename="/task1-0909/">
