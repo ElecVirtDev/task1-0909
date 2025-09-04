4311c640663df76198ec60d6a3716983bc06412c
ElecVirtDev <evd94623@gmail.com>
2025-09-04 15:51:04 +0900

    build(deps): update package-lock.json, package.json and main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 //CSS 불러오기
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/css/bootstrap-icons.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./assets/css/font-awesome-pro.css";
 import "./assets/css/animate.css";
 import "./assets/css/glightbox.min.css";
@@ -17,18 +16,18 @@ import "./assets/css/responsive.css";
 //JS 불러오기
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "jquery/dist/jquery.min.js";
-import "./js/jquery.waypoints.min.js";
-import "./js/jquery.counterup.min.js";
-import "./js/modernizr.min.js";
-import "./js/popper.min.js";
-import "./js/mixitup.min.js";
-import "./js/glightbox.min.js";
-import "./js/vanilla-tilt.min.js";
-import "./js/swiper-bundle.js";
-import "./js/scrollCue.min.js";
-import "./js/wow.js";
-import "./js/mobile-menu.js";
-import "./js/main.js";
+import "./assets/js/jquery.waypoints.min.js";
+import "./assets/js/jquery.counterup.min.js";
+import "./assets/js/modernizr.min.js";
+import "./assets/js/popper.min.js";
+import "./assets/js/mixitup.min.js";
+import "./assets/js/glightbox.min.js";
+import "./assets/js/vanilla-tilt.min.js";
+import "./assets/js/swiper-bundle.js";
+import "./assets/js/scrollCue.min.js";
+import "./assets/js/wow.js";
+import "./assets/js/mobile-menu.js";
+import "./assets/js/main.js";
 
 //JSX 불러오기
 import App from "./App.jsx";
