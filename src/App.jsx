cece32e6aabc95f16628a3d4dbf30755d42fac0a
ElecVirtDev <evd94623@gmail.com>
2025-09-04 15:46:48 +0900

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-<<<<<<< HEAD
-=======
-import "./assets/css/app.css";
->>>>>>> b95e59c7bafdbe014e1328f1aebebc68f8e3c4e1
 
 function App() {
   const [count, setCount] = useState(0);
