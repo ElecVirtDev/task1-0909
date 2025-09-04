4d9aac9c79bde28255243cfcdd94a9ac4288ff53
ElecVirtDev <evd94623@gmail.com>
2025-09-04 15:46:25 +0900

    update 11 files and delete 14 files

--- src/main.jsx
+++ src/main.jsx
@@ -16,20 +16,19 @@ import "./assets/css/responsive.css";
 
 //JS 불러오기
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "jquery/dist/jquery.min.js";
+import "./js/jquery.waypoints.min.js";
+import "./js/jquery.counterup.min.js";
 import "./js/modernizr.min.js";
-import "./js/jquery-3.6.3.min.js";
 import "./js/popper.min.js";
 import "./js/mixitup.min.js";
 import "./js/glightbox.min.js";
-import "./js/jquery.waypoints.min.js";
-import "./js/jquery.counterup.min.js";
 import "./js/vanilla-tilt.min.js";
 import "./js/swiper-bundle.js";
 import "./js/scrollCue.min.js";
 import "./js/wow.js";
 import "./js/mobile-menu.js";
 import "./js/main.js";
-import "./assets/css/index.css";
 
 //JSX 불러오기
 import App from "./App.jsx";
