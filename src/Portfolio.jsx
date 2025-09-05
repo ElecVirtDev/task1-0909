dba816da5f4a3f8c38a4ec692ee89184025beae3
ElecVirtDev <evd94623@gmail.com>
2025-09-05 12:33:00 +0900

    update Portfolio.jsx

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -3,17 +3,32 @@ export default function Portfolio() {
     <div className="portfolio-cate-section section-padding-2">
       <div className="container">
         <div className="row">
-          {/* 제목 */}
           <div className="col-lg-6 mb-30 align-self-center">
             <div className="ct-section-title text-left">
-              <h4>SELECTED PROJECTS</h4>
-              <h2>
+              <h4
+                className="wow fadeInUp"
+                data-wow-delay=".2s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: "0.2s",
+                  animationName: "fadeInUp",
+                }}
+              >
+                SELECTED PROJECTS
+              </h4>
+              <h2
+                className="wow fadeInUp"
+                data-wow-delay=".4s"
+                style={{
+                  visibility: "visible",
+                  animationDelay: "0.4s",
+                  animationName: "fadeInUp",
+                }}
+              >
                 Digital Case <span>Studies.</span>
               </h2>
             </div>
           </div>
-
-          {/* 카테고리 필터 */}
           <div className="col-lg-6 align-self-center">
             <div className="portfolio-cate-list-full mb-40 text-right">
               <ul>
@@ -21,7 +36,7 @@ export default function Portfolio() {
                   All
                 </li>
                 <li data-filter=".cloud">Cloud</li>
-                <li data-filter=".data">Data Analysis</li>
+                <li data-filter=".data">Data Analaysis</li>
                 <li data-filter=".mobile">Mobile Apps</li>
                 <li data-filter=".technology">Technology</li>
               </ul>
@@ -29,56 +44,391 @@ export default function Portfolio() {
           </div>
         </div>
 
-        {/* 프로젝트 리스트 */}
-        <div className="row p-projects-full">
+        <div className="row p-projects-full portF" id="MixItUpB20995">
           <div className="col-lg-4 col-md-6 col-12 mb-30 mix data">
-            <div className="portfolio-item-single">
-              <div className="thumbnail">
-                <img src="./assets/imgs/imgs/p-1.png" alt="portfolio" />
+            <div
+              className="portfolio-item-single"
+              data-cues="slideInUp"
+              data-disabled="true"
+            >
+              <div
+                className="thumbnail"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "0ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <img src="./assets/imgs/p-1.png" alt="portfolio" />
               </div>
-              <div className="zoom-icon">
+              <div
+                className="zoom-icon"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "0ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
                 <a
                   className="image-popup"
-                  href="./assets/imgs/imgs/p-1.png"
                   data-gallery="portfolio1"
+                  href="https://preetheme.com/html/techpro/assets/img/portfolio/p-1.png"
                 >
                   <i className="fa-solid fa-plus"></i>
                 </a>
               </div>
-              <div className="content">
+              <div
+                className="content"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "0ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
                 <h2>
-                  <a href="/portfolio-details">Effective Solutions</a>
+                  <a href="https://preetheme.com/html/techpro/portfolio-details.html">
+                    Effective Solutions
+                  </a>
                 </h2>
-                <strong>Data Analysis</strong>
+                <strong>Data Analaysis</strong>
               </div>
             </div>
           </div>
-
-          {/* 프로젝트 카드 패턴 반복 */}
           <div className="col-lg-4 col-md-6 col-12 mb-30 mix cloud mobile">
-            <div className="portfolio-item-single">
-              <div className="thumbnail">
-                <img src="./assets/imgs/imgs/p-2.png" alt="portfolio" />
+            <div
+              className="portfolio-item-single"
+              data-cues="slideInUp"
+              data-disabled="true"
+            >
+              <div
+                className="thumbnail"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "300ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <img src="./assets/imgs/p-2.png" alt="portfolio" />
               </div>
-              <div className="zoom-icon">
+              <div
+                className="zoom-icon"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "300ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
                 <a
                   className="image-popup"
-                  href="./assets/imgs/imgs/p-2.png"
                   data-gallery="portfolio1"
+                  href="https://preetheme.com/html/techpro/assets/img/portfolio/p-2.png"
                 >
                   <i className="fa-solid fa-plus"></i>
                 </a>
               </div>
-              <div className="content">
+              <div
+                className="content"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "300ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
                 <h2>
-                  <a href="/portfolio-details">Effective Solutions</a>
+                  <a href="https://preetheme.com/html/techpro/portfolio-details.html">
+                    Effective Solutions
+                  </a>
                 </h2>
-                <strong>Cloud, Mobile</strong>
+                <strong>Data Analaysis</strong>
               </div>
             </div>
           </div>
-
-          {/* 추가 포트폴리오 카드도 동일 구조로 반복 */}
+          <div className="col-lg-4 col-md-6 col-12 mb-30 mix technology">
+            <div
+              className="portfolio-item-single"
+              data-cues="slideInUp"
+              data-disabled="true"
+            >
+              <div
+                className="thumbnail"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "600ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <img src="./assets/imgs/p-3.png" alt="portfolio" />
+              </div>
+              <div
+                className="zoom-icon"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "600ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <a
+                  className="image-popup"
+                  data-gallery="portfolio1"
+                  href="https://preetheme.com/html/techpro/assets/img/portfolio/p-3.png"
+                >
+                  <i className="fa-solid fa-plus"></i>
+                </a>
+              </div>
+              <div
+                className="content"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "600ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <h2>
+                  <a href="https://preetheme.com/html/techpro/portfolio-details.html">
+                    Effective Solutions
+                  </a>
+                </h2>
+                <strong>Data Analaysis</strong>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-12 mb-30 mix mobile">
+            <div
+              className="portfolio-item-single"
+              data-cues="slideInUp"
+              data-disabled="true"
+            >
+              <div
+                className="thumbnail"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "0ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <img src="./assets/imgs/p-4.png" alt="portfolio" />
+              </div>
+              <div
+                className="zoom-icon"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "0ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <a
+                  className="image-popup"
+                  data-gallery="portfolio1"
+                  href="https://preetheme.com/html/techpro/assets/img/portfolio/p-4.png"
+                >
+                  <i className="fa-solid fa-plus"></i>
+                </a>
+              </div>
+              <div
+                className="content"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "0ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <h2>
+                  <a href="https://preetheme.com/html/techpro/portfolio-details.html">
+                    Effective Solutions
+                  </a>
+                </h2>
+                <strong>Data Analaysis</strong>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-12 mb-30 mix cloud">
+            <div
+              className="portfolio-item-single"
+              data-cues="slideInUp"
+              data-disabled="true"
+            >
+              <div
+                className="thumbnail"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "300ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <img src="./assets/imgs/p-5.png" alt="portfolio" />
+              </div>
+              <div
+                className="zoom-icon"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "300ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <a
+                  className="image-popup"
+                  data-gallery="portfolio1"
+                  href="https://preetheme.com/html/techpro/assets/img/portfolio/p-5.png"
+                >
+                  <i className="fa-solid fa-plus"></i>
+                </a>
+              </div>
+              <div
+                className="content"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "0ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <h2>
+                  <a href="https://preetheme.com/html/techpro/portfolio-details.html">
+                    Effective Solutions
+                  </a>
+                </h2>
+                <strong>Data Analaysis</strong>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4 col-md-6 col-12 mb-30 mix data">
+            <div
+              className="portfolio-item-single"
+              data-cues="slideInUp"
+              data-disabled="true"
+            >
+              <div
+                className="thumbnail"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "600ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <img src="./assets/imgs/p-6.png" alt="portfolio" />
+              </div>
+              <div
+                className="zoom-icon"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "600ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <a
+                  className="image-popup"
+                  data-gallery="portfolio1"
+                  href="https://preetheme.com/html/techpro/assets/img/portfolio/p-6.png"
+                >
+                  <i className="fa-solid fa-plus"></i>
+                </a>
+              </div>
+              <div
+                className="content"
+                data-cue="slideInUp"
+                data-show="true"
+                style={{
+                  animationName: "slideInUp",
+                  animationDuration: "500ms",
+                  animationTimingFunction: "ease",
+                  animationDelay: "600ms",
+                  animationDirection: "normal",
+                  animationFillMode: "both",
+                }}
+              >
+                <h2>
+                  <a href="https://preetheme.com/html/techpro/portfolio-details.html">
+                    Effective Solutions
+                  </a>
+                </h2>
+                <strong>Data Analaysis</strong>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
