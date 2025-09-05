264d55bae907677dec1c437cf38e8e7762ede99c
ElecVirtDev <evd94623@gmail.com>
2025-09-05 11:33:34 +0900

    update 8 files and delete 4 files

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -21,7 +21,7 @@ export default function HeroSection() {
                 <div className="video-btn">
                   <a
                     className="video-p-btn"
-                    href="https://www.youtube.com/watch?v=kD_Db3TCno0&t=60s"
+                    href="https://www.youtube.com/watch?v=kD_Db3TCno0&amp;t=60s"
                   >
                     <i className="fas fa-play"></i>
                   </a>
@@ -30,8 +30,17 @@ export default function HeroSection() {
             </div>
           </div>
           <div className="col-lg-6 align-self-center">
-            <div className="image" data-tilt data-tilt-scale="1.05">
-              <img src="./assets/imgs/rimg01.webp" alt="hero" />
+            <div
+              className="image"
+              data-tilt=""
+              data-tilt-scale="1.05"
+              style={{
+                willChange: "transform",
+                transform:
+                  "perspective(1000px) rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)",
+              }}
+            >
+              <img src="./assets/imgs/rimg01.webp" alt="hero image" />
             </div>
           </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { createPopper } from "@popperjs/core";
-import Swiper from "swiper/bundle";
+import WOW from "wow.js";
+import ScrollCue from "scrollcue";
 
 // CSS 불러오기
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import "./assets/css/swiper-bundle.css";
 import "./assets/css/font-awesome-pro.css";
-import "./assets/css/animate.css";
+import "animate.css";
 import "./assets/css/glightbox.min.css";
 import "./assets/css/scrollCue.css";
 import "./assets/css/spacing.css";
@@ -16,15 +17,17 @@ import "./assets/css/responsive.css";
 import "./assets/css/main.css";
 
 // JS 불러오기
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./components/js/jquery-3.6.3.min.js";
+import "./components/js/popper.min.js";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./components/js/mixitup.min.js";
 import "./components/js/glightbox.min.js";
 import "./components/js/jquery.waypoints.min.js";
 import "./components/js/jquery.counterup.min.js";
 import "./components/js/vanilla-tilt.min.js";
-import "./components/js/scrollCue.min.js";
+import "./components/js/swiper-bundle.js";
 import "./components/js/mobile-menu.js";
+import "./components/js/main.js";
 
 // JSX 불러오기
 import Preloader from "./Preloader";
@@ -40,6 +43,19 @@ import Banner from "./Banner";
 import Team from "./Team";
 import Testimonials from "./Testimonials";
 import Footer from "./Footer";
+
+const App = () => {
+  useEffect(() => {
+    // main.js 파일을 동적으로 import
+    import("./components/js/main.js")
+      .then(() => {
+        console.log("main.js has been loaded");
+      })
+      .catch((error) => {
+        console.error("Error loading main.js:", error);
+      });
+  }, []);
+};
 
 // React DOM 렌더링
 ReactDOM.createRoot(document.getElementById("root")).render(
